ccaa622d0bbcb2e9f124a01a314336708f2c1b05
Amir <amirdavis23x@gmail.com>
2023-04-27 11:40:24 -0400

    Added fragment_lecture_slides.xml and lecture slides

---
+++ app/src/main/java/com/example/mobile_app_project_v1/lectureSlides/LectureSlideOneFragment.kt
@@ -0,0 +1,45 @@
+package com.example.mobile_app_project_v1.lectureSlides
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mobile_app_project_v1.R
+import com.example.mobile_app_project_v1.databinding.FrameBinding
+
+class LectureSlideOneFragment : Fragment(R.layout.frame) {
+    //private var _binding: ActivityMainBinding = null
+    private lateinit var binding: FrameBinding
+    private val STORAGE_PERMISSION_CODE: Int = 1000
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    //private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FrameBinding.inflate(inflater, container, false)
+
+        val textView = binding.textView
+        textView.text = getString(R.string.lecture1)
+        //getString(R.id.)
+
+        //These were used before viewBinding
+        //val v = inflater.inflate(R.layout.frame, container, false)
+        //val pdf = v.findViewById<PDFView>(R.id.pdfview)
+
+        val pdf = binding.pdfview
+        pdf.fromAsset("Introduction_ Android Development with Kotlin.pdf").load()
+
+        val buttonDownload = binding.buttonDownload
+        buttonDownload.setOnClickListener {
+            // Handle button click event
+
+            //downloadPdfFromAssets()
+        }
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/mobile_app_project_v1/lectureSlides/LectureSlideTwoFragment.kt
@@ -0,0 +1,40 @@
+package com.example.mobile_app_project_v1.lectureSlides
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mobile_app_project_v1.R
+import com.example.mobile_app_project_v1.databinding.FrameBinding
+
+class LectureSlideTwoFragment : Fragment(R.layout.frame) {
+    //private var _binding: ActivityMainBinding = null
+    private lateinit var binding: FrameBinding
+    private val STORAGE_PERMISSION_CODE: Int = 1000
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    //private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FrameBinding.inflate(inflater, container, false)
+
+        val textView = binding.textView
+        textView.text = getString(R.string.lectureslide2)
+
+        val pdf = binding.pdfview
+        pdf.fromAsset("Lesson 2_ Functions.pdf").load()
+
+        val buttonDownload = binding.buttonDownload
+        buttonDownload.setOnClickListener {
+            // Handle button click event
+
+            //downloadPdfFromAssets()
+        }
+        return binding.root
+    }
+}
